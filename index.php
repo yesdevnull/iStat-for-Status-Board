46cfc9c52564ca09cef0d313c4bea4a115fe0141
Dan Barrett <dan@theblahman.net>
2013-04-12 23:10:38 +1000

    added switch statement for multiple commands

--- index.php
+++ index.php
@@ -1,59 +1,45 @@
 <?php
 
-$db = new PDO ( 'sqlite:/Library/Application Support/iStat Server/databases/local.db' );
+$data = $_GET['data'];
 
-$sql = 'SELECT user, system, time FROM hour_cpuhistory ORDER BY time DESC LIMIT 20';
-
-$finalArray = array (
-	'graph' => array (
-		'title' => 'CPU History' ,
-		'type' => 'bar' ,
-		'datasequences' => '' ,
-	)
-);
-
-
-foreach ( $db->query ( $sql ) as $row ) {
-	//echo 'Time: ' . $row['time'] . ' &mdash; User: ' . $row['user'] . ' CPU: ' . $row['system'] . "\n";
-	$cpu_user[] = $row['user'];
-	$cpu_system[] = $row['system'];
+switch ( $data ) {
+	case 'cpu_hour' :
+	
+		$db = new PDO ( 'sqlite:/Library/Application Support/iStat Server/databases/local.db' );
+		
+		$sql = 'SELECT user, system, time FROM hour_cpuhistory ORDER BY time DESC LIMIT 20';
+		
+		$finalArray = array (
+			'graph' => array (
+				'title' => 'CPU History' ,
+				'type' => 'bar' ,
+				'datasequences' => '' ,
+			)
+		);
+		
+		
+		foreach ( $db->query ( $sql ) as $row ) {
+			$cpu_user[] = $row['user'];
+			$cpu_system[] = $row['system'];
+		}
+		
+		
+		$finalArray['graph']['datasequences'] = array (
+			array (
+				'title' => 'User' ,
+				'color' => 'red' ,
+				'datapoints' => $cpu_user ,
+			) ,
+			array (
+				'title' => 'System' ,
+				'color' => 'green' ,
+				'datapoints' => $cpu_system ,
+			)
+		);
+		
+	break;
 }
-
-// print_r ( $cpu_user );
-
-// $finalArray['graph']['datasequences'] = 'cock';
-
-
-$finalArray['graph']['datasequences'] = array (
-	array (
-		'title' => 'User' ,
-		'color' => 'red' ,
-		'datapoints' => $cpu_user ,
-	) ,
-	array (
-		'title' => 'System' ,
-		'color' => 'green' ,
-		'datapoints' => $cpu_system ,
-	)
-);
 
 header ( 'content-type: application/json' );
 
-//print_r ( $finalArray );
-
 echo json_encode ( $finalArray );
-
-
-/*
-
-$result = $db->query ( $sql );
-
-$row = $result->fetch();
-
-//print_r ( $row );
-echo $row['time'] . "\n\n";
-
-echo date ( 'd-m-Y H:i:s' , $row['time'] ) . "\n\n";
-
-echo time();
-*/
