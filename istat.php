168a433f6908b594f63ab698339278dc77061ad2
Dan Barrett <dan@theblahman.net>
2013-05-16 21:07:24 +1000

    Fixed up graph object - it no longer crashes Status Board

--- istat.php
+++ istat.php
@@ -4,26 +4,29 @@ $data = $_GET['data'];
 
 switch ( $data ) {
 	case 'cpu_hour' :
-	
+
 		$db = new PDO ( 'sqlite:/Library/Application Support/iStat Server/databases/local.db' );
-		
-		$sql = 'SELECT user, system, time FROM hour_cpuhistory ORDER BY time DESC LIMIT 20';
-		
+
+		$sql = 'SELECT user, system, time FROM hour_cpuhistory ORDER BY time DESC LIMIT 10';
+
 		$finalArray = array (
 			'graph' => array (
 				'title' => 'CPU History' ,
-				'type' => 'bar' ,
+				'type' => 'line' ,
 				'datasequences' => '' ,
 			)
 		);
-		
-		
+
+
 		foreach ( $db->query ( $sql ) as $row ) {
-			$cpu_user[] = $row['user'];
-			$cpu_system[] = $row['system'];
+			$time = date ( 'H:i:s' ,  $row['time'] );
+			
+			$cpu_user[] = array ( 'title' => (string) $time , 'value' => $row['user'] );
+			
+			$cpu_system[] = array ( 'title' => (string) $time , 'value' => $row['system'] );
 		}
-		
-		
+
+
 		$finalArray['graph']['datasequences'] = array (
 			array (
 				'title' => 'User' ,
@@ -36,10 +39,11 @@ switch ( $data ) {
 				'datapoints' => $cpu_system ,
 			)
 		);
-		
+
 	break;
 }
 
 header ( 'content-type: application/json' );
-
+//echo '<pre>';
+//print_r ( $finalArray );
 echo json_encode ( $finalArray );
