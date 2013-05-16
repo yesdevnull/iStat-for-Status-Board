51282031293653f723dfd11c75ef6c10234eb55a
Dan Barrett <dan@theblahman.net>
2013-05-16 22:09:19 +1000

    Added RAM hourly history and major changes to graph objects for nicer output.

--- istat.php
+++ istat.php
@@ -2,17 +2,110 @@
 
 $data = $_GET['data'];
 
+// From: http://stackoverflow.com/a/5501447
+function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2);
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2);
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2);
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+}
+
 switch ( $data ) {
 	case 'cpu_hour' :
 
 		$db = new PDO ( 'sqlite:/Library/Application Support/iStat Server/databases/local.db' );
 
-		$sql = 'SELECT user, system, time FROM hour_cpuhistory ORDER BY time DESC LIMIT 10';
+		$sql = 'SELECT user, system, time, nice FROM hour_cpuhistory ORDER BY time DESC LIMIT 20';
 
 		$finalArray = array (
 			'graph' => array (
-				'title' => 'CPU History' ,
+				'title' => 'CPU History - Hourly' ,
 				'type' => 'line' ,
+				'refreshEveryNSeconds' => '30' ,
+				'datasequences' => '' ,
+				'yAxis' => array (
+					'minValue' => 0 ,
+					'maxValue' => 100 ,
+					'units' => array (
+						'suffix' => '%' ,
+					) ,
+				) ,
+			)
+		);
+
+
+		foreach ( $db->query ( $sql ) as $row ) {
+			$time = date ( 'H:i:s' ,  $row['time'] );
+			
+			$cpu_user[] = array ( 'title' => $time , 'value' => $row['user'] );
+			
+			$cpu_system[] = array ( 'title' => $time , 'value' => $row['system'] + $row['user'] );
+			
+			$cpu_nice[] = array ( 'title' => $time , 'value' => $row['nice'] + $row['system'] + $row['user'] );
+		}
+
+
+		$finalArray['graph']['datasequences'] = array (
+			array (
+				'title' => 'System' ,
+				'color' => 'green' ,
+				'datapoints' => $cpu_system ,
+			) ,
+			array (
+				'title' => 'Nice' ,
+				'color' => 'blue' ,
+				'datapoints' => $cpu_nice ,
+			) ,
+			array (
+				'title' => 'User' ,
+				'color' => 'red' ,
+				'datapoints' => $cpu_user ,
+			) ,
+		);
+
+	break;
+	
+	case 'ram_hour' :
+
+		$db = new PDO ( 'sqlite:/Library/Application Support/iStat Server/databases/local.db' );
+
+		$sql = 'SELECT wired, active, time, inactive, free, total FROM hour_memoryhistory ORDER BY time DESC LIMIT 20';
+
+		$finalArray = array (
+			'graph' => array (
+				'title' => 'RAM History - Hourly' ,
+				'type' => 'line' ,
+				'refreshEveryNSeconds' => '30' ,
+				'yAxis' => array (
+					'minValue' => 0 ,
+					'maxValue' => formatSizeUnits ( '8388608.000000' ) ,
+					'units' => array (
+						'suffix' => ' GB' ,
+					)
+				) ,
 				'datasequences' => '' ,
 			)
 		);
@@ -21,29 +114,42 @@ switch ( $data ) {
 		foreach ( $db->query ( $sql ) as $row ) {
 			$time = date ( 'H:i:s' ,  $row['time'] );
 			
-			$cpu_user[] = array ( 'title' => (string) $time , 'value' => $row['user'] );
+			$ram_wired[] = array ( 'title' => $time , 'value' => formatSizeUnits ( $row['wired'] ) );
 			
-			$cpu_system[] = array ( 'title' => (string) $time , 'value' => $row['system'] );
+			$ram_active[] = array ( 'title' => $time , 'value' => formatSizeUnits ( $row['active'] + $row['wired'] ) );
+			
+			$ram_inactive[] = array ( 'title' => $time , 'value' => formatSizeUnits ( $row['inactive'] + $row['active'] + $row['wired'] ) );
+			
+			$ram_free[] = array ( 'title' => $time , 'value' => formatSizeUnits ( $row['free'] + $row['inactive'] + $row['active'] + $row['wired'] ) );
 		}
 
 
 		$finalArray['graph']['datasequences'] = array (
 			array (
-				'title' => 'User' ,
-				'color' => 'red' ,
-				'datapoints' => $cpu_user ,
+				'title' => 'Free' ,
+				'color' => 'yellow' ,
+				'datapoints' => $ram_free ,
 			) ,
 			array (
-				'title' => 'System' ,
+				'title' => 'Inactive' ,
+				'color' => 'blue' ,
+				'datapoints' => $ram_inactive ,
+			) ,
+			array (
+				'title' => 'Active' ,
 				'color' => 'green' ,
-				'datapoints' => $cpu_system ,
-			)
+				'datapoints' => $ram_active ,
+			) ,
+			array (
+				'title' => 'Wired' ,
+				'color' => 'red' ,
+				'datapoints' => $ram_wired ,
+			) ,
 		);
-
+	
 	break;
 }
 
 header ( 'content-type: application/json' );
-//echo '<pre>';
-//print_r ( $finalArray );
+
 echo json_encode ( $finalArray );
